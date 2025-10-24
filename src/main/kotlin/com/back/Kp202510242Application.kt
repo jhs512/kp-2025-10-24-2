ad020fe00b9c876241a5d9f769282792780d127e
jhs512 <jangka512@gmail.com>
2025-10-24 11:17:40 +0900

    first

---
+++ src/main/kotlin/com/back/Kp202510242Application.kt
@@ -0,0 +1,11 @@
+package com.back
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class Kp202510242Application
+
+fun main(args: Array<String>) {
+    runApplication<Kp202510242Application>(*args)
+}
